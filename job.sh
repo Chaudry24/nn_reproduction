41f6ca32866b29a0eeef26ace14656b1cfccefb9
Muhammad Chaudry <machaudr@sabine.rcdc.uh.edu>
2021-12-01 13:09:21 -0600

    debug things

--- job.sh
+++ job.sh
@@ -5,9 +5,9 @@
 #SBATCH -N 1 -n 1
 #SBATCH -p gpu
 #SBATCH --gres=gpu:1
-#SBATCH --mem=32GB
+#SBATCH --mem=64GB
 #SBATCH --mail-user=machaudry@uh.edu
 #SBATCH --mail-type=END
 
 module load python/3.9
-python -u some_file_3.py
+python -u some_file_4.py
