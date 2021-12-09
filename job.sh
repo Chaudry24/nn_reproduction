bf52c4fd20fc02c1b3c9627b2759c66f6842572a
Muhammad Chaudry <machaudr@sabine.rcdc.uh.edu>
2021-12-08 20:41:40 -0600

    changes

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J mle_nn_job
 #SBATCH -o mle_nn.o%j
-#SBATCH -t 24:00:00
+#SBATCH -t 00:10:00
 #SBATCH -N 1 -n 1
 #SBATCH -p gpu
 #SBATCH --gres=gpu:1
@@ -9,4 +9,4 @@
 #SBATCH --mail-user=machaudry@uh.edu
 #SBATCH --mail-type=END
 
-/project/jun/machaudr/miniconda3.9/envs/myenv/python -u some_file_6.py
+/project/jun/machaudr/miniconda3.9/envs/myenv/bin/python -u some_file_4.py
