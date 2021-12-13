2adc4f3d209e4b7e7c494efc5433366b44deb121
Muhammad Chaudry <machaudr@sabine.rcdc.uh.edu>
2021-12-13 15:46:05 -0600

    test data generated

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J data_generatio_job
 #SBATCH -o data_generation.o%j
-#SBATCH -t 01:00:00
+#SBATCH -t 00:10:00
 #SBATCH -N 1 -n 1
 ##SBATCH -p gpu
 ##SBATCH --gres=gpu:1
@@ -9,4 +9,4 @@
 #SBATCH --mail-user=machaudry@uh.edu
 #SBATCH --mail-type=END
 
-/project/jun/machaudr/miniconda3.9/envs/myenv/bin/python -u data_generation.py
+/project/jun/machaudr/miniconda3.9/envs/myenv/bin/python -u data_generation_my_idea.py
