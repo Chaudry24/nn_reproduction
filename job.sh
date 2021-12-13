c764495f2e8092259c6e3507fb9061eec6ad7d38
Muhammad Chaudry <machaudr@sabine.rcdc.uh.edu>
2021-12-13 15:31:12 -0600

    untrack large files

--- job.sh
+++ job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -J mle_nn_job
-#SBATCH -o mle_nn.o%j
-#SBATCH -t 96:00:00
+#SBATCH -J data_generatio_job
+#SBATCH -o data_generation.o%j
+#SBATCH -t 01:00:00
 #SBATCH -N 1 -n 1
-#SBATCH -p gpu
-#SBATCH --gres=gpu:1
+##SBATCH -p gpu
+##SBATCH --gres=gpu:1
 #SBATCH --mem=64GB
 #SBATCH --mail-user=machaudry@uh.edu
 #SBATCH --mail-type=END
 
-/project/jun/machaudr/miniconda3.9/envs/myenv/bin/python -u some_file_4.py
+/project/jun/machaudr/miniconda3.9/envs/myenv/bin/python -u data_generation.py
